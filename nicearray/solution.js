71015b9e2f284cde32d9fefb6eb33d04de0a0013
billodell <billodell5@gmail.com>
2017-12-04 19:42:48 -0800

    nice array

--- nicearray/solution.js
+++ nicearray/solution.js
@@ -1,10 +1,11 @@
 function isNice(arr){
-    let flag = true;
-     arr.forEach(function(i) {
-        if ((arr.includes(i + 1))||(arr.includes(i - 1))) {
-        console.log(i)
-      } else {
-        flag = false}
-        })
-      return flag
-    }
+  let flag = true;
+  if (arr.length<1){
+    flag = false}
+   arr.forEach(function(i) {
+      if ((arr.includes(i + 1))||(arr.includes(i - 1))) {
+    } else {
+      flag = false}
+      })
+    return flag
+  }
